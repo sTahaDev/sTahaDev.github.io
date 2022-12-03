e0a78d0db3d12f374988d57e5cf3aead4dc52093
Taha <105307821+sTahaDev@users.noreply.github.com>
2022-12-03 20:04:11 +0300

    main dosyalar
    main dosyalar

---
+++ app.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $("#eror").hide();
+
+    var isim = $("#firstName").val();
+    var soyisim = $("#lastName").val();
+    var mail = $("#email").val();
+    var adres;
+    var sehir;
+    var ilce;
+    var not;
+    var siparisEkle = (isim,soyisim,mail,adres,sehir,ilce,not) => {
+        var boslukvar= false;
+        var liste = [isim,soyisim,mail,adres,sehir,ilce,not];
+        for (let i = 0; i < liste.length; i++) {
+            if(liste[i] == "" || liste[i] == " " || liste[i] == "  " || liste[i] == "   "){
+                boslukvar = true;
+                
+            }
+            
+        }
+        if(boslukvar){
+            $("#eror").show();
+            window.scrollTo(0, 0);
+        }
+        else{
+            $("#eror").hide();
+            alert(liste);
+        }
+       
+      
+    }
+    $("#butonn").click(function(){
+        
+        isim = $("#firstName").val();
+        soyisim = $("#lastName").val();
+        mail = $("#email").val();
+        adres = $("#address").val();
+        sehir = $("#country").val();
+        ilce = $("#state").val();
+        not = $("#not").val();
+        
+        siparisEkle("Siparisin Verildi: ",isim,soyisim,mail,adres,sehir,ilce,not);
+        
+        
+    })
+
+    
+    
+})
