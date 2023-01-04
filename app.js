dc7066ff41e70134ede94f243bb65cc9a46859fe
Taha <105307821+sTahaDev@users.noreply.github.com>
2023-01-04 17:44:20 +0300

    bitcord 0.1

--- app.js
+++ app.js
@@ -1,49 +1,12 @@
+
 $(document).ready(function(){
-    $("#eror").hide();
-
-    var isim = $("#firstName").val();
-    var soyisim = $("#lastName").val();
-    var mail = $("#email").val();
-    var adres;
-    var sehir;
-    var ilce;
-    var not;
-    var siparisEkle = (isim,soyisim,mail,adres,sehir,ilce,not) => {
-        var boslukvar= false;
-        var liste = [isim,soyisim,mail,adres,sehir,ilce,not];
-        for (let i = 0; i < liste.length; i++) {
-            if(liste[i] == "" || liste[i] == " " || liste[i] == "  " || liste[i] == "   "){
-                boslukvar = true;
-                
-            }
-            
-        }
-        if(boslukvar){
-            $("#eror").show();
-            window.scrollTo(0, 0);
-        }
-        else{
-            $("#eror").hide();
-            alert("Siparisin Verildi: "+liste);
-        }
-       
-      
-    }
-    $("#butonn").click(function(){
-        
-        isim = $("#firstName").val();
-        soyisim = $("#lastName").val();
-        mail = $("#email").val();
-        adres = $("#address").val();
-        sehir = $("#country").val();
-        ilce = $("#state").val();
-        not = $("#not").val();
-        
-        siparisEkle(isim,soyisim,mail,adres,sehir,ilce,not);
-        
-        
-    })
-
+  
+  let mesaj_gonder = (User,mesaj) => {
     
+    $(".userslist").children().append('<li><input type="image" src="'+User.pp+'" width="50px"></li>')
     
-})
+  }
+
+  mesaj_gonder(user[1])
+});
+
---
+++ users.js
@@ -0,0 +1,18 @@
+let user = [];
+
+user[0] = {
+    "name": "Hasan",
+    "surname": "boz",
+    "yas": "19",
+    "pp": "./src/img/defPP.png"
+    
+
+}
+user[1] = {
+    "name": "Taha",
+    "surname": "sahin",
+    "yas": "19",
+    "pp": "https://whatifgaming.com/wp-content/uploads/2021/10/d9f9eb49a49935e2a6779fe06bdb4b8b59306226.png"
+    
+
+}
