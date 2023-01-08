3cafc593a55c4ef4d872d5f264f3ef6d15984cb4
Taha <105307821+sTahaDev@users.noreply.github.com>
2023-01-08 14:28:31 +0300

    bitcord 1.3

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 
 $(document).ready(function(){
+  $(".me").hide();
+  $("#kabulbtn").click(function (e) { 
+    $("#namebg").hide();
+    $(".me").show();
+    var bipVoice = new Audio('./src/audio/discord_notification.mp3')
   
-  let mesaj_gonder = (User,mesaj) => {
+    let mesaj_gonder = (User,mesaj,time) => {
+      
+      
+      
+      setTimeout(function(){
+        bipVoice.play();
+        
+        $(".userslist").append('<li><input type="image" src="'+User.pp+'" width="50px" id="'+User.id+'"></li>  ')
+        
+        $(".userslist").children("li").children("input").click(function (e) { 
+        
+        if ($(this).attr("id") == "1"){
+          
+          
+          $(".messageprofile").find(".mesaage").children().text(mesaj);
+        }
+        
+        
+        });
+      },time)
+      
+      
+      
+      
+    }
     
-    $(".userslist").children().append('<li><input type="image" src="'+User.pp+'" width="50px"></li>')
     
-  }
-
-  mesaj_gonder(user[1])
+    mesaj_gonder(user[1],"adam ol222",2000)
+    mesaj_gonder(user[2],"hihihha",3000)
+      
+  });
+  
 });
 
--- users.js
+++ users.js
@@ -1,6 +1,7 @@
 let user = [];
 
 user[0] = {
+    "id":"0",
     "name": "Hasan",
     "surname": "boz",
     "yas": "19",
@@ -9,10 +10,20 @@ user[0] = {
 
 }
 user[1] = {
+    "id":"1",
     "name": "Taha",
     "surname": "sahin",
     "yas": "19",
     "pp": "https://whatifgaming.com/wp-content/uploads/2021/10/d9f9eb49a49935e2a6779fe06bdb4b8b59306226.png"
+    
+
+}
+user[2] = {
+    "id":"2",
+    "name": "Taha",
+    "surname": "sahin",
+    "yas": "19",
+    "pp": "https://pbs.twimg.com/media/EphOvxpXEAIdn-l.jpg"
     
 
 }
